26a8565101d2285d288f465bb6de8a2c4ad8115e
Andrew Lachance <waffls@yahoo.com>
2025-08-21 22:44:52 -0400

    Added more args for plugin

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -7,11 +7,20 @@ import { writeFileSync } from "fs";
 // const args = {
 //   [arg1]: arg1V,
 //   [arg2]: arg2V
+//   [arg3]: arg3V
+//   [arg4]: arg4V
 // }
 
-export default function plugin(
-  baseDirectory: string, 
-  lambdaName: string
-) {
-    getAllFilePaths(baseDirectory).forEach(filePath => writeFileSync(filePath, replace(lambdaName, filePath)))
+export default function plugin({
+  baseDirectory,
+  lambdaName,
+  importSource,
+  importSpecifier,
+}: {
+  baseDirectory: string,
+  lambdaName: string,
+  importSource: string,
+  importSpecifier: string,
+}) {
+  getAllFilePaths(baseDirectory).forEach(filePath => writeFileSync(filePath, replace(lambdaName, filePath, importSource, importSpecifier)))
 }
