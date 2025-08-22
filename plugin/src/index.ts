480ae309f5238370b2c158c1d3b0a6642768503c
Andrew Lachance <waffls@yahoo.com>
2025-08-21 22:39:42 -0400

    Added comment

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -2,6 +2,13 @@ import replace from "./function-replacer";
 import { getAllFilePaths } from "./file-utils";
 import { writeFileSync } from "fs";
 
+// const [arg1, arg1V, arg2, arg2V] = process.argv.slice(-4)
+
+// const args = {
+//   [arg1]: arg1V,
+//   [arg2]: arg2V
+// }
+
 export default function plugin(
   baseDirectory: string, 
   lambdaName: string
